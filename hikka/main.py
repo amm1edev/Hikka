6adc6cc97225c3937b9b89a8bce01c6fc8084e2c
amm1e <88576648+amm1edev@users.noreply.github.com>
2025-01-12 08:44:24 +0600

    Update main.py

--- hikka/main.py
+++ hikka/main.py
@@ -784,8 +784,8 @@ class Hikka:
                 logging.getLogger().handlers[0].get_logid_by_client(client.tg_id),
                 "https://0x0.st/8-4Z.mp4#nsfw",
                 caption=(
-                    "<emoji document_id=5264913427041631157>🦋</emoji> <b>Limoka {} started!</b>\n\n",
-                    "<emoji document_id=5265113787265988883>🦋</emoji> <b>Fork Developer: </b>thislyomi.t.me",
+                    "🦋 <b>Limoka {} started!</b>\n\n",
+                    "🦋 <b>Fork Developer: </b>thislyomi.t.me",
                         upd,
                         web_url,
                     )
