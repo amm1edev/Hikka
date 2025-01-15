d1cc2f1144b45b167f444b5152c4e8d1455c9871
dienelox <dolyakypiaezy@gmail.com>
2025-01-15 17:05:15 +0300

    bye
    Co-Authored-By: amm1e <88576648+amm1edev@users.noreply.github.com>

--- hikka/version.py
+++ hikka/version.py
@@ -5,7 +5,7 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-__version__ = (testeri,go,naxyi)
+__version__ = (6,6,6)
 
 import os
 
